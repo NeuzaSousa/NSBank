f3542c0c82894a9370e6b703c10bbb37a706cf08
NeuzaSousa <neuzarssousa@gmail.com>
2022-10-19 18:07:10 +0200

    changes debit

--- src/components/Debit.java
+++ src/components/Debit.java
@@ -1,5 +1,11 @@
 package components;
 
+import java.util.Date;
+
 public class Debit {
 
+	protected Debit(String comment, int identifier, double ammount, int targetAccountNumber, boolean effect,
+			Date dateOfFlow) {
+		super();
+	}
 }
